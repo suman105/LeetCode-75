10171871014c42291e0f85c692aa87aecc9ea193
suman105 <majjarisuman77@gmail.com>
2025-02-07 12:07:05 -0600

    pushing queue problems

---
+++ Queue/649 Dota2 Senate.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> radiant, dire; 
+        int n = senate.size();
+
+        for (int i = 0; i < n; i++) {
+            if (senate[i] == 'R') radiant.push(i);
+            else dire.push(i);
+        }
+        
+        while (!radiant.empty() && !dire.empty()) {
+            int r = radiant.front(), d = dire.front();
+            radiant.pop();
+            dire.pop();
+            
+            if (r < d) {
+                radiant.push(r + n);
+            } else {
+                dire.push(d + n);
+            }
+        }
+        
+        return radiant.empty() ? "Dire" : "Radiant";
+    }
+};
---
+++ Queue/933 Number of Recent Calls.cpp
@@ -0,0 +1,17 @@
+class RecentCounter {
+private:
+    queue<int> requests;
+
+public:
+    RecentCounter() {}
+
+    int ping(int t) {
+        requests.push(t);
+
+        while (!requests.empty() && requests.front() < t - 3000) {
+            requests.pop();
+        }
+
+        return requests.size();
+    }
+};
